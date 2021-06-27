18934cd1d9669992c35fc7360966ef2f9a5a012e
Lars Åkerlund <lars.akerlund@redeploy.com>
2021-06-27 18:05:13 +0200

    Updated module dependency

--- resources.appliance.tf
+++ resources.appliance.tf
@@ -11,7 +11,7 @@ resource "azurerm_availability_set" "appliance" {
 
 module "appliance" {
   for_each = local.appliance_config
-  source   = "github.com/RedeployAB/terraform-azurerm-fortigate-single?ref=v0.1.0"
+  source   = "github.com/RedeployAB/terraform-azurerm-fortigate-single?ref=v0.2.0"
 
   name                = each.value.name
   resource_group_name = var.resource_group_name
